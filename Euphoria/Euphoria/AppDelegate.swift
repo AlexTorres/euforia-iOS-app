78312c3e7c5b4029a3a13c1286a290d5ca873945
John Torres <johntorres1@thepublicgroup.com>
2016-10-16 18:30:09 -0500

    classes Added

--- Euphoria/Euphoria/AppDelegate.swift
+++ Euphoria/Euphoria/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    GMSServices.provideAPIKey(Constans.GoogleKeys.maps)
+    GMSPlacesClient.provideAPIKey(Constans.GoogleKeys.maps)
     return true
   }
 
---
+++ Euphoria/Euphoria/Modules/MotelList/User Interface/View/MapCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MapCollectionViewCell.swift
+//  Euphoria
+//
+//  Created by John Torres  on 10/16/16.
+//  Copyright © 2016 John Torres . All rights reserved.
+//
+
+import UIKit
+
+class MapCollectionViewCell: UICollectionViewCell {
+    
+}
